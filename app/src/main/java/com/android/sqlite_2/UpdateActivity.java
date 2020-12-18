e8ec6c58c99ee4d7cabc65b9d230dda9db88db55
kyeongmipak <qkrrudal7@naver.com>
2020-12-18 17:21:09 +0900

    update add_kyeongmi

--- app/src/main/java/com/android/sqlite_2/UpdateActivity.java
+++ app/src/main/java/com/android/sqlite_2/UpdateActivity.java
@@ -3,13 +3,77 @@ package com.android.sqlite_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class UpdateActivity extends Activity {
+
+    final static String TAG = "Status";
+
+    Button btnUpdate;
+    EditText studentName, studentMajor, studentTel;
+    StudentInfo studentInfo;
+    String name, major, tel;
+    int studentId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        studentInfo = new StudentInfo(UpdateActivity.this);
+
+        studentName = findViewById(R.id.studentname_update);
+        studentMajor = findViewById(R.id.studentmajor_update);
+        studentTel = findViewById(R.id.studenttel_update);
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra("studentname");
+        major = intent.getStringExtra("studentmajor");
+        tel = intent.getStringExtra("studenttel");
+        studentId = intent.getIntExtra("studentid",0);
+
+        studentName.setText(name);
+        studentMajor.setText(major);
+        studentTel.setText(tel);
+
+        findViewById(R.id.btnUpdate).setOnClickListener(onClickListener);
+
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+
+        SQLiteDatabase DB;
+
+        @Override
+        public void onClick(View v) {
+
+            name = studentName.getText().toString();
+            major = studentMajor.getText().toString();
+            tel = studentTel.getText().toString();
+
+            try{
+                DB = studentInfo.getWritableDatabase();
+
+                String query = "UPDATE student SET studentname = '" + name + "', studentmajor = '" + major;
+                String query1 = "', studenttel = '" + tel + "' WHERE studentid = " + Integer.toString(studentId) + ";";
+                DB.execSQL(query+query1);
+
+                studentInfo.close();
+                Toast.makeText(UpdateActivity.this, "학생 정보가 수정되었습니다.", Toast.LENGTH_SHORT).show();
+
+            } catch (Exception e){
+                e.printStackTrace();
+                Toast.makeText(UpdateActivity.this, "오류가 발생하였습니다.", Toast.LENGTH_SHORT).show();
+            }
+
+            Intent intent = new Intent(UpdateActivity.this, SelectActivity.class);
+            startActivity(intent);
+        }
+    };
 }
