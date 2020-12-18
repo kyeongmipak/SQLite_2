9da8fd047c05b388876fe8750fb88c98cf3c0e96
RiaSong <donna_s@sookmyung.ac.kr>
2020-12-18 17:38:51 +0900

    12/18 BackUp_OnResume

--- app/src/main/java/com/android/sqlite_2/SelectActivity.java
+++ app/src/main/java/com/android/sqlite_2/SelectActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
@@ -21,11 +22,13 @@ public class SelectActivity extends Activity {
     private StudentAdapter adapter = null;
     private ListView listView = null;
     private SQLiteDatabase DB;
+    Bundle savedInstanceState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+
 
         StudentInfo studentInfo;
         studentInfo = new StudentInfo(SelectActivity.this);
@@ -95,39 +98,41 @@ public class SelectActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        onCreate(savedInstanceState);
 
-        StudentInfo studentInfo;
-        studentInfo = new StudentInfo(SelectActivity.this);
-        try{                // 읽어올 때는 readable사용!
-            DB = studentInfo.getReadableDatabase();
-            // 차레대로 0, 1, 2 순서에 있음.
-            String query = "SELECT * FROM student;";
 
-            Cursor cursor =  DB.rawQuery(query, null);
-
-            // String보다 퍼포먼스 빠른거!
-            StringBuffer  stringBuffer = new StringBuffer();
-
-            // 데이터를 하나씩 읽으면서 이동해라!
-            while (cursor.moveToNext()){
-                // 0, 1, 2에 맞게 불러와줘!
-                String studentid = cursor.getString(0);
-                String studentname = cursor.getString(1);
-                String studentmajor = cursor.getString(2);
-                String studenttel = cursor.getString(3);
-
-                // 불러온거를 stringBuffer에 담자!
-                //stringBuffer.append("studentid: " + studentid + "\nstudentname : " + studentname + "\nstudentmajor : " + studentmajor + "\nstudenttel : " + studenttel + "\n----------------------------------");
-                data.add(new StudentBean(studentid, studentname, studentmajor, studenttel));
-            }
-            listView = findViewById(R.id.lv_student);
-            listView.setAdapter(adapter);
-            cursor.close();
-            studentInfo.close();
-
-        } catch(Exception e){
-            e.printStackTrace();
-        }
+//        StudentInfo studentInfo;
+//        studentInfo = new StudentInfo(SelectActivity.this);
+//        try{                // 읽어올 때는 readable사용!
+//            DB = studentInfo.getReadableDatabase();
+//            // 차레대로 0, 1, 2 순서에 있음.
+//            String query = "SELECT * FROM student;";
+//
+//            Cursor cursor =  DB.rawQuery(query, null);
+//
+//            // String보다 퍼포먼스 빠른거!
+//            StringBuffer  stringBuffer = new StringBuffer();
+//
+//            // 데이터를 하나씩 읽으면서 이동해라!
+//            while (cursor.moveToNext()){
+//                // 0, 1, 2에 맞게 불러와줘!
+//                String studentid = cursor.getString(0);
+//                String studentname = cursor.getString(1);
+//                String studentmajor = cursor.getString(2);
+//                String studenttel = cursor.getString(3);
+//
+//                // 불러온거를 stringBuffer에 담자!
+//                //stringBuffer.append("studentid: " + studentid + "\nstudentname : " + studentname + "\nstudentmajor : " + studentmajor + "\nstudenttel : " + studenttel + "\n----------------------------------");
+//                data.add(new StudentBean(studentid, studentname, studentmajor, studenttel));
+//            }
+//            listView = findViewById(R.id.lv_student);
+//            listView.setAdapter(adapter);
+//            cursor.close();
+//            studentInfo.close();
+//
+//        } catch(Exception e){
+//            e.printStackTrace();
+//        }
 
 
 
