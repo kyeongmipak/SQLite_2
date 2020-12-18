f1d5f99bdb604dd69c3ef821bab2a42759b3aabe
mg6613 <mg6613@naver.com>
2020-12-18 19:42:11 +0900

    Yoo activity_splash.xml AndroidManifest.xml jordy.jpeg MainActivity.java SplashActivity.java
    Update.

--- app/src/main/java/com/android/sqlite_2/SplashActivity.java
+++ app/src/main/java/com/android/sqlite_2/SplashActivity.java
@@ -66,7 +66,7 @@ public class SplashActivity extends Activity {
                 startActivity(intent);
                 finish();
             }
-        },8000);
+        },3000);
     }
 
     @Override
